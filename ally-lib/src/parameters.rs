86ea14e0046d82c60fce7cf53920528e9978bf24
Jonathan DeCarlo <Jonathan.DeCarlo@bentley.com>
2022-04-02 03:15:49 -0400

    Cleaned up the code a bit more and error some basic accounting for errors.

--- ally-lib/src/parameters.rs
+++ ally-lib/src/parameters.rs
@@ -1,28 +1,30 @@
 use clap::{Command, Arg, ArgMatches};
 use yaml_rust::YamlLoader;
 
-pub fn parse_command_line_args_from_yaml_string(yaml_string: &str) -> ArgMatches {
-    let docs = YamlLoader::load_from_str(yaml_string).unwrap();
+pub fn parse_command_line_args_from_yaml_string(yaml_string: &str) -> Result<ArgMatches, i32> {
+    let docs = YamlLoader::load_from_str(yaml_string).map_err(| _e | { 0 })?;
     let doc = &docs[0];
     let command_data = &doc["command"];
     
-    let mut cmd = Command::new(command_data["name"].as_str().unwrap());
+    let mut cmd = Command::new(command_data["name"].as_str().ok_or(0)?);
     
-    let args_data = command_data["args"].as_vec().unwrap();
+    let args_data = command_data["args"].as_vec().ok_or(0)?;
     for arg_data in args_data {
         let arg_properties = &arg_data["arg"];
-        let mut arg = Arg::new(arg_properties["name"].as_str().unwrap());
+        let mut arg = Arg::new(arg_properties["name"].as_str().ok_or(0)?);
 
-        if !arg_properties["short"].is_badvalue() {
-            arg = arg.short(arg_properties["short"].as_str().unwrap().chars().next().unwrap().to_owned());
+        let short_arg = &arg_properties["short"];
+        if !short_arg.is_badvalue() {
+            arg = arg.short(short_arg.as_str().ok_or(0)?.chars().nth(0).ok_or(0)?);
         }
 
-        if !arg_properties["long"].is_badvalue() {
-            arg = arg.long(arg_properties["long"].as_str().unwrap());
+        let long_arg = &arg_properties["long"];
+        if !long_arg.is_badvalue() {
+            arg = arg.long(long_arg.as_str().ok_or(0)?);
         }
 
         cmd = cmd.arg(arg);
     }
     
-    cmd.get_matches()
+    Ok(cmd.get_matches())
 }
--- ally/src/main.rs
+++ ally/src/main.rs
@@ -4,6 +4,6 @@ use ally_lib::parameters;
 fn main() {
     let yaml_string = include_str!("args.yaml");
 
-    let matches = parameters::parse_command_line_args_from_yaml_string(yaml_string);
+    let matches = parameters::parse_command_line_args_from_yaml_string(yaml_string).unwrap_or_default();
     println!("{:?}", matches);
 }
