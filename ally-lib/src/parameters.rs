7fecdcdfbb49a12d9190272f92c0abcaab6f555d
Jonathan DeCarlo <Jonathan.DeCarlo@bentley.com>
2022-04-04 16:46:45 -0400

    Added the rest of the properties for args. Commands next.

--- ally-lib/src/parameters.rs
+++ ally-lib/src/parameters.rs
@@ -1,5 +1,6 @@
+use std::cell::RefCell;
 use clap::{Command, Arg, ArgMatches};
-use yaml_rust::YamlLoader;
+use yaml_rust::{YamlLoader, Yaml};
 
 pub fn parse_command_line_args_from_yaml_string(yaml_string: &str) -> Result<ArgMatches, i32> {
     let docs = YamlLoader::load_from_str(yaml_string).map_err(| _e | { 0 })?;
@@ -11,55 +12,130 @@ pub fn parse_command_line_args_from_yaml_string(yaml_string: &str) -> Result<Arg
     let args_data = command_data["args"].as_vec().ok_or(0)?;
     for arg_data in args_data {
         let arg_properties = &arg_data["arg"];
-        let mut arg = Arg::new(arg_properties["name"].as_str().ok_or(0)?);
+        let arg = RefCell::new(Arg::new(arg_properties["id"].as_str().ok_or(0)?));
 
-        let short_arg = &arg_properties["short"];
-        if !short_arg.is_badvalue() {
-            arg = arg.short(short_arg.as_str().ok_or(0)?.chars().nth(0).ok_or(0)?);
-        }
+        set_arg_property(&arg, &arg_properties["alias"], Arg::alias, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["allow_hyphen_values"], Arg::allow_hyphen_values, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["allow_invalid_utf8"], Arg::allow_invalid_utf8, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["conflicts_with"], Arg::conflicts_with, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["default_missing_value"], Arg::default_missing_value, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["default_value"], Arg::default_value, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["display_order"], Arg::display_order, | property | {
+            if let Some(i) = property.as_i64() {
+                Some(i as usize)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["env"], Arg::env, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["exclusive"], Arg::exclusive, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["forbid_empty_values"], Arg::forbid_empty_values, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["global"], Arg::global, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["group"], Arg::group, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["help"], Arg::help, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["hide"], Arg::hide, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["hide_default_value"], Arg::hide_default_value, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["hide_env"], Arg::hide_env, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["hide_long_help"], Arg::hide_long_help, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["hide_possible_values"], Arg::hide_possible_values, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["hide_short_help"], Arg::hide_short_help, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["ignore_case"], Arg::ignore_case, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["index"], Arg::index, | property | {
+            if let Some(i) = property.as_i64() {
+                Some(i as usize)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["last"], Arg::last, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["long"], Arg::long, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["long_help"], Arg::long_help, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["max_occurrences"], Arg::max_occurrences, | property | {
+            if let Some(i) = property.as_i64() {
+                Some(i as usize)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["max_values"], Arg::max_values, | property | {
+            if let Some(i) = property.as_i64() {
+                Some(i as usize)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["min_values"], Arg::min_values, | property | {
+            if let Some(i) = property.as_i64() {
+                Some(i as usize)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["multiple_occurrences"], Arg::multiple_occurrences, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["multiple_values"], Arg::multiple_values, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["next_line_help"], Arg::next_line_help, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["number_of_values"], Arg::number_of_values, | property | {
+            if let Some(i) = property.as_i64() {
+                Some(i as usize)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["overrides_with"], Arg::overrides_with, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["possible_value"], Arg::possible_value, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["raw"], Arg::raw, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["require_equals"], Arg::require_equals, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["require_value_delimiter"], Arg::require_value_delimiter, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["required"], Arg::required, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["required_unless_present"], Arg::required_unless_present, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["requires"], Arg::requires, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["short"], Arg::short, | property | {
+            if let Some(s) = property.as_str() {
+                s.chars().nth(0)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["short_alias"], Arg::short_alias, | property | {
+            if let Some(s) = property.as_str() {
+                s.chars().nth(0)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["takes_value"], Arg::takes_value, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["use_value_delimiter"], Arg::use_value_delimiter, Yaml::as_bool)?;
+        set_arg_property(&arg, &arg_properties["value_delimiter"], Arg::value_delimiter, | property | {
+            if let Some(s) = property.as_str() {
+                s.chars().nth(0)
+            } else {
+                None
+            }
+        })?;
+        set_arg_property(&arg, &arg_properties["value_name"], Arg::value_name, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["value_terminator"], Arg::value_terminator, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["visible_alias"], Arg::visible_alias, Yaml::as_str)?;
+        set_arg_property(&arg, &arg_properties["visible_short_alias"], Arg::visible_short_alias, | property | {
+            if let Some(s) = property.as_str() {
+                s.chars().nth(0)
+            } else {
+                None
+            }
+        })?;
 
-        let long_arg = &arg_properties["long"];
-        if !long_arg.is_badvalue() {
-            arg = arg.long(long_arg.as_str().ok_or(0)?);
-        }
-
-        let value_name_arg = &arg_properties["value_name"];
-        if !value_name_arg.is_badvalue() {
-            arg = arg.value_name(value_name_arg.as_str().ok_or(0)?);
-        }
-
-        let help_arg = &arg_properties["help"];
-        if !help_arg.is_badvalue() {
-            arg = arg.help(help_arg.as_str().ok_or(0)?);
-        }
-
-        let long_help_arg = &arg_properties["long_help"];
-        if !long_help_arg.is_badvalue() {
-            arg = arg.long_help(long_help_arg.as_str().ok_or(0)?);
-        }
-
-        let takes_value_arg = &arg_properties["takes_value"];
-        if !takes_value_arg.is_badvalue() {
-            arg = arg.takes_value(takes_value_arg.as_bool().ok_or(0)?);
-        }
-
-        let index_arg = &arg_properties["index"];
-        if !index_arg.is_badvalue() {
-            arg = arg.index(index_arg.as_i64().ok_or(0)? as usize);
-        }
-
-        let hide_arg = &arg_properties["hide"];
-        if !hide_arg.is_badvalue() {
-            arg = arg.hide(hide_arg.as_bool().ok_or(0)?);
-        }
-
-        let conflicts_with_arg = &arg_properties["conflicts_with"];
-        if !conflicts_with_arg.is_badvalue() {
-            arg = arg.conflicts_with(conflicts_with_arg.as_str().ok_or(0)?);
-        }
-
-        cmd = cmd.arg(arg);
+        cmd = cmd.arg(arg.borrow().to_owned());
     }
     
     Ok(cmd.get_matches())
+}
+
+fn set_arg_property<'a, FnSetArgVal, FnGetPropVal, TProp>(arg: &RefCell<Arg<'a>>, property: &'a Yaml, set_arg_val: FnSetArgVal, get_property_val: FnGetPropVal) -> Result<(), i32>
+    where FnSetArgVal: FnOnce(Arg<'a>, TProp) -> Arg<'a>,
+    FnGetPropVal: FnOnce(&'a Yaml) -> Option<TProp> {
+    if !property.is_badvalue() {
+        let owned_arg = arg.borrow().to_owned();
+        *arg.borrow_mut() = set_arg_val(owned_arg, get_property_val(property).ok_or(0)?);
+    }
+
+    Ok(())
 }
